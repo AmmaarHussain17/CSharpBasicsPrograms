05fd77e0e941a7500bd7a19d2f2399d04c56abaf
Mohammed Ammaar Hussain <164994848+AmmaarHussain17@users.noreply.github.com>
2026-01-29 12:08:39 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,140 @@
+﻿using ManualStringProcessing;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var data = "    employee name :    JOHN   doe   \n" +
+        "    EMPLOYEE id:    0045  \n" +
+        "    Department  :      finance  \n" +
+        "joining date  :12   -   03   -2021  \n" +
+        "email :     John.Doe@Company.COM\n " +
+        "phone number:     91 - 98765   43210 \n" +
+        "remarks :      excellent   performer \n" +
+        "remarks :needs    improvement\n" +
+        "END  of      report\n";
+
+
+
+        // 1. lines
+        // 2. leading trailing spaces => |employee name :    JOHN   doe|
+        // 3. separating lines by colon(key:value) => |employee name |                            
+        //                                            | JOHN   doe|
+        // 4. trailing spaces => |
+        // 4. Converting all sentences into lower cases
+        // 5. Adding an Underscore_ in keys.
+
+
+
+        string[] lines = SeperatingSentences.Lines(data);
+
+        string[] eachKeyData = new string[lines.Length];
+        string[] eachValuesData = new string[lines.Length];
+
+        int indexesKeyForBreakingKeyAndValuesInEachSentence = 0;
+        int indexesValuesForBreakingKeyAndValuesInEachSentence = 0;
+        foreach (string trimmed in lines)
+        {
+            MyKeyValue eachKeyAndValueData = SeperatingSentences.BreakingKeyAndValuesInEachSentence(trimmed);
+            eachKeyData[indexesKeyForBreakingKeyAndValuesInEachSentence++] = eachKeyAndValueData.key;
+            eachValuesData[indexesValuesForBreakingKeyAndValuesInEachSentence++] = eachKeyAndValueData.value;
+        }
+
+
+        string[] afterTrimmedStringKeys = new string[lines.Length];
+        int indexesForSeperatingSentencesKeys = 0;
+
+        foreach (string keyLines in eachKeyData)
+        {
+            string trimmedLineKey = SeperatingSentences.LeadingSpacesTrimmed(keyLines);
+            afterTrimmedStringKeys[indexesForSeperatingSentencesKeys++] = trimmedLineKey;
+        }
+
+
+        string[] afterTrimmedStringValues = new string[lines.Length];
+        int indexesForSeperatingSentencesValues = 0;
+        foreach (string ValueLines in eachValuesData)
+        {
+            string trimmedLineKey = SeperatingSentences.LeadingSpacesTrimmed(ValueLines);
+            afterTrimmedStringValues[indexesForSeperatingSentencesValues++] = trimmedLineKey;
+        }
+
+        string[] afterTrimmedStringKeysEnd = new string[lines.Length];
+        int indexesForSeperatingSentencesKeysEnd = 0;
+
+        foreach (string keyLinesEnd in afterTrimmedStringKeys)
+        {
+            string trimmedLineKeyEnds = SeperatingSentences.trailingSpacesTrimmed(keyLinesEnd);
+            afterTrimmedStringKeysEnd[indexesForSeperatingSentencesKeysEnd++] = trimmedLineKeyEnds;
+        }
+
+        string[] afterTrimmedStringValuesEnd = new string[lines.Length];
+        int indexesForSeperatingSentencesValuesEnd = 0;
+
+        foreach (string valuesLinesEnd in afterTrimmedStringValues)
+        {
+            string trimmedLineValuesEnds = SeperatingSentences.trailingSpacesTrimmed(valuesLinesEnd);
+            afterTrimmedStringValuesEnd[indexesForSeperatingSentencesValuesEnd++] = trimmedLineValuesEnds;
+        }
+
+        foreach (string data3 in afterTrimmedStringValuesEnd)
+        {
+            Console.Write(data3);
+            Console.WriteLine();
+        }
+
+        string[] keysAfterConvertedIntoLowercase = new string[lines.Length];
+        int indexesValuesAfterConvertedIntoLowercaseForKeys = 0;
+        foreach (string lowercaseForKeyData in eachKeyData)
+        {
+            string afterConvertedIntoLowercase = SeperatingSentences.ConvertingSentencesIntoLowercase(lowercaseForKeyData);
+            keysAfterConvertedIntoLowercase[indexesValuesAfterConvertedIntoLowercaseForKeys++] = afterConvertedIntoLowercase;
+        }
+
+        string[] valuesAfterConvertedIntoLowercase = new string[lines.Length];
+        int indexesValuesAfterConvertedIntoLowercaseForValues = 0;
+        foreach (string lowercaseForKeyData in eachKeyData)
+        {
+            string afterConvertedIntoLowercase = SeperatingSentences.ConvertingSentencesIntoLowercase(lowercaseForKeyData);
+            valuesAfterConvertedIntoLowercase[indexesValuesAfterConvertedIntoLowercaseForValues++] = afterConvertedIntoLowercase;
+        }
+
+
+
+
+        //string[] keysAfterAddedUnderscore = new string[lines.Length];
+        //int indexesValuesAfterAddedUnderscoreForKeys = 0;
+        //foreach (string keyBeforeAddedUnderScore in keysAfterConvertedIntoLowercase)
+        //{
+        //    string keyAfterAddedUnderScore = SeperatingSentences.addAnUnderscoreInKeys(keyBeforeAddedUnderScore);
+        //    keysAfterAddedUnderscore[indexesValuesAfterAddedUnderscoreForKeys++] = keyAfterAddedUnderScore;
+        //}
+        //foreach (string dataTemp in keysAfterAddedUnderscore)
+        //{
+        //    Console.WriteLine(dataTemp);
+        //}
+
+
+
+
+        //foreach (string dataTemp in valuesAfterConvertedIntoLowercase)
+        //{
+        //    Console.WriteLine(dataTemp);
+        //}
+        //Console.Write($"{eachKeyData[0]} ");
+        //Console.Write($"{eachValuesData[0]} ");
+        //SeperatingSentences.sentencesStrings(data);
+
+
+        //string data = "abc\n" +
+        //              "ghf\n";
+
+
+
+        //foreach(string eachData in eachDetail)
+        //{
+        //    Console.WriteLine(eachData);
+        //}
+
+    }
+}
---
+++ SeperatingSentences.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace ManualStringProcessing
+{
+    public class MyKeyValue
+    {
+        public string key;
+        public string value;
+    }
+    internal class SeperatingSentences
+    {
+        public static string[] Lines(string data)
+        {
+            int countSizeOfEachString = 0;
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i] == '\n') countSizeOfEachString++;
+            }
+
+            string[] eachLine = new string[countSizeOfEachString];
+            int nextLineStartingStringLength = 0;
+
+            for (int i = 0; i < eachLine.Length; i++)
+            {
+                int j = nextLineStartingStringLength;
+                while (j < data.Length)
+                {
+                    if (data[j] == '\n') break;
+
+                    eachLine[i] += data[j];
+
+                    j++;
+                }
+                nextLineStartingStringLength = j + 1;
+            }
+            return eachLine;
+        }
+        
+        public static MyKeyValue BreakingKeyAndValuesInEachSentence(string keyAndValueString)
+        {
+            string eachKey = null;
+            string eachValue = null;
+
+            MyKeyValue keyValue = new MyKeyValue();
+
+            int namesValesStartingIndex = 0;
+
+            for (int i = 0; i < keyAndValueString.Length; i++)
+            {
+                if (keyAndValueString[i] == ':')
+                {
+                    namesValesStartingIndex = i + 1;
+                    break;
+                }
+                eachKey += keyAndValueString[i];
+            }
+            for (int k = namesValesStartingIndex; k < keyAndValueString.Length; k++)
+            {
+                eachValue += keyAndValueString[k];
+            }
+
+            keyValue.key = eachKey;
+            keyValue.value = eachValue;
+
+            return keyValue;
+        }
+
+        public static string LeadingSpacesTrimmed(string line)
+        {
+            string eachLine = null;
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (line[i] != ' ')
+                {
+                    for (int j = i; j < line.Length; j++)
+                    {
+                        eachLine += line[i++];
+                    }
+                    break;
+                }
+            }
+            return eachLine;
+        }
+
+        public static string trailingSpacesTrimmed(string line)
+        {
+            string eachLine = null;
+            int k = 0;
+
+            for (int i = line.Length - 1; i >= 0 ; i--)
+            {
+                if (line[i] != ' ')
+                {
+                    for (int j = 0; j <= i; j++)
+                    {
+                        eachLine += line[k++];
+                    }
+                    break;
+                }
+            }
+            return eachLine;
+        }
+        public static string ConvertingSentencesIntoLowercase(string eachSentenceData)
+        {
+            string lowercaseConvertedEachLine = null;
+            for(int i = 0; i < eachSentenceData.Length; i++)
+            {
+                if (eachSentenceData[i] >= 'A' && eachSentenceData[i] <= 'Z')
+                    lowercaseConvertedEachLine += (char) (eachSentenceData[i] + 32);
+                else
+                    lowercaseConvertedEachLine += eachSentenceData[i];
+            }
+            return lowercaseConvertedEachLine;
+        }
+
+        //public static string addAnUnderscoreInKeys(string eachKeySentence)
+        //{
+        //    string keyAfterAddedUnderScore = null;
+        //    for(int i = 0; i < eachKeySentence.Length - 1; i++)
+        //    {
+        //        if (eachKeySentence[i] == ' ')
+        //            keyAfterAddedUnderScore += '_';
+        //        else
+        //            keyAfterAddedUnderScore += eachKeySentence[i];
+        //    }
+        //    return keyAfterAddedUnderScore;
+        //}
+
+
+
+        //public static void sentencesStrings(string data)
+        //{
+        //    int countSizeOfEachString = 0;
+        //    for (int i = 0; i < data.Length; i++)
+        //    {
+        //        if (data[i] == '\n') countSizeOfEachString++;
+        //    }
+
+        //    string[] eachLine = new string[countSizeOfEachString];
+        //    int nextLineStartingStringLength = 0;
+
+        //    for (int i = 0; i < eachLine.Length; i++)
+        //    {
+        //        int j = nextLineStartingStringLength;
+        //        while (j < data.Length)
+        //        {
+        //            if (data[j] == '\n') break;
+
+        //            eachLine[i] += data[j];
+
+        //            j++;
+        //        }
+        //        nextLineStartingStringLength = j + 1;
+        //    }
+
+        //    string[] eachFieldHeader = new string[countSizeOfEachString];
+        //    string[] eachFieldNames = new string[countSizeOfEachString];
+
+
+        //    
+
+        //    string[] namesAfterConvertingIntoLowercase = convertIntoLowercase(eachFieldNames);
+        //    string[] headersAfterConvertingIntoLowercase = convertIntoLowercase(eachFieldHeader);
+
+        //    underscoreAdderAtHeader(headersAfterConvertingIntoLowercase);
+
+        //    //underscoreAdder(eachFieldHeader);
+
+        //}
+        //public static string[] convertIntoLowercase(string[] headerConvertIntoLowerCase)
+        //{
+        //    for (int i = 0; i < headerConvertIntoLowerCase.Length; i++)
+        //    {
+        //        char[] chars = headerConvertIntoLowerCase[i].ToCharArray();
+
+        //        for (int j = 0; j < chars.Length; j++)
+        //        {
+        //            if (chars[j] >= 'A' && chars[j] <= 'Z')
+        //            {
+        //                chars[j] = (char)(chars[j] + 32);
+        //            }
+        //        }
+        //        headerConvertIntoLowerCase[i] = new string(chars);
+
+        //    }
+        //    //foreach (string temp in headerConvertIntoLowerCase)
+        //    //{
+        //    //    Console.WriteLine(temp);
+        //    //}
+        //    return headerConvertIntoLowerCase;
+        //}
+        //public static void underscoreAdderAtHeader(string[] adderUnderscoreInHeaders)
+        //{
+        //    for (int str = 0; str < adderUnderscoreInHeaders.Length; str++)
+        //    {
+
+        //    }
+        //foreach (string temp in adderUnderscoreInHeaders)
+        //{
+        //    Console.WriteLine(temp);
+        //}
+
+    }
+}
